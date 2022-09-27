d0ad62f7c069cf7b51b9392d61daaab14ed9db9b
Sam Avery <sam.avery1@gmail.com>
2022-09-27 10:47:21 -0500

    commit tree 15f3e0cfb01950ad35342685c09ede96ff938bf5

--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import QuestionItem from "./QuestionItem"
 
 function QuestionList() {
+
+  const [questions, setQuestions] = useState([])
+
+  useEffect(() => {
+    fetch('http://localhost:4000/questions')
+    .then((r) => r.json())
+    .then((questions) => setQuestions(questions))
+    }, [])
+    console.log(questions)
+
+  let questionList = questions.map((question) => {
+    return (
+      <QuestionItem
+      key={question.id}
+      question={question}
+        />
+    )
+  })
+  
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>
+        {questionList}
+      </ul>
     </section>
   );
 }
